f3735d869f09229ce0908ed600e0b9688f3b607e
Mike Nelson <nelmedia@gmail.com>
2019-10-31 08:46:27 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,6 @@
-$Shell=$Host.UI.RawUI
-$size=$Shell.WindowSize
-$size.width=120
-$size.height=50
-$Shell.WindowSize=$size
-$Shell.CursorSize=10
+function Get-ChildItem-Format-Wide 
+{
+  $New_Args = @($true)
+  $New_Args += "$Args"
+  Invoke-Expression -Command "Get-ChildItem-Color $New_Args"
+}
