cff7ce638d8a7f9167d95a288f8c61a74fd1ce4e
Mike Nelson <mike@mikenelson.io>
2025-04-02 12:36:30 -0500

    Update hostfileedit.ps1

--- hostfileedit.ps1
+++ hostfileedit.ps1
@@ -1,4 +1,8 @@
-﻿$file = Join-Path -Path $($env:windir) -ChildPath "system32\drivers\etc\hosts"            
+# Author: Mike Nelson
+
+# Identifiable text: This is what the agent should "see" in the script. I am using the word WhiskeyTangoFoxtrotDelta or WTFD for identification.
+
+ $file = Join-Path -Path $($env:windir) -ChildPath "system32\drivers\etc\hosts"            
  if (-not (Test-Path -Path $file)){            
    Throw "Hosts file not found"            
  }            
